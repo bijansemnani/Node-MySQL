2e386f3b6a60e3ebf712138585126ff1679582fb
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-15 21:07:06 -0700

    added view products for sale functionality

--- Bamazon-Manager/bamazonManager.js
+++ Bamazon-Manager/bamazonManager.js
@@ -14,3 +14,48 @@ var connection = mysql.createConnection({
   password: "",
   database: "bamazonDB"
 });
+
+function read(query, clean, funky) {
+  console.log(query);
+  console.log(clean);
+  connection.query(query, clean, function (err, response) {
+    if(err) throw err;
+    funky(response);
+  })
+}
+
+function recurPrompt(){
+  inquire.prompt([
+    {
+      type:"list",
+      name:"choice",
+      message:"What would you like to do?",
+      choices:["View Products for Sale",
+               "View Low Inventory",
+               "Add to Inventory",
+               "Add New Product"
+              ]
+    }
+  ]).then(function (answer) {
+    console.log(answer);
+    var query;
+    switch (answer.choice) {
+      case "View Products for Sale":
+        query = "SELECT * FROM products";
+        read(query, null, function (data) {
+          for (var i = 1; i < data.length; i++) {
+            console.log("Item: " + data[i].product_name
+                    + ", Department: " + data[i].department_name
+                    + ", Price: $" + data[i].price
+                    + ", Quantity: " + data[i].stock_quantity);
+            console.log("----------------------------------------------------------------------");        
+          }
+        });
+        break;
+      default:
+
+    }
+  });
+}
+
+recurPrompt();
